84875e5ef64f059dd3e3acb3d7f75938cacfd1bf
Davidelos <davidelosacco06@gmail.com>
2025-11-26 12:35:04 +0100

    formattato output

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -1,7 +1,30 @@
 #include "protocol.h"
 
+static int send_n(int sock, const char* buffer, size_t length, int flags) {
+    size_t total_sent = 0;
+    int bytes_sent_this_turn;
+    while (total_sent < length) {
+        bytes_sent_this_turn = send(sock, buffer + total_sent, length - total_sent, flags);
+        if (bytes_sent_this_turn <= 0) return bytes_sent_this_turn;
+        total_sent += bytes_sent_this_turn;
+    }
+    return total_sent;
+}
+
+static int recv_n(int sock, char* buffer, size_t length, int flags) {
+    size_t total_received = 0;
+    int bytes_received_this_turn;
+    while (total_received < length) {
+        bytes_received_this_turn = recv(sock, buffer + total_received, length - total_received, flags);
+        if (bytes_received_this_turn <= 0) return bytes_received_this_turn;
+        total_received += bytes_received_this_turn;
+    }
+    return total_received;
+}
+
+
 int main(int argc, char *argv[]) {
-    setvbuf(stdout, NULL, _IONBF, 0); // Disabilita buffering output
+    setvbuf(stdout, NULL, _IONBF, 0);
 
     char server_ip[CITY_LEN] = DEFAULT_IP;
     int port = DEFAULT_PORT;
@@ -9,7 +32,7 @@ int main(int argc, char *argv[]) {
     memset(&req, 0, sizeof(req));
     int request_provided = 0;
 
-    // Parsing Argomenti
+    // Parsing Argomenti (rimosso per brevità)
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
             port = atoi(argv[i + 1]);
@@ -21,14 +44,12 @@ int main(int argc, char *argv[]) {
         } else if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
             char* input_str = argv[i + 1];
             req.type = input_str[0];
-            char* space_ptr = strchr(input_str, ' ');
 
+            char* space_ptr = strchr(input_str, ' ');
             if (space_ptr != NULL) {
                 strncpy(req.city, space_ptr + 1, CITY_LEN - 1);
-            } else {
-                if (strlen(input_str) > 1) {
-                    strncpy(req.city, input_str + 1, CITY_LEN - 1);
-                }
+            } else if (strlen(input_str) > 1) {
+                strncpy(req.city, input_str + 1, CITY_LEN - 1);
             }
             req.city[CITY_LEN - 1] = '\0';
             request_provided = 1;
@@ -37,23 +58,18 @@ int main(int argc, char *argv[]) {
     }
 
     if (!request_provided) {
-        printf("Errore: Parametro -r obbligatorio.\nUso: %s -s <IP> -p <PORTA> -r \"<t|h|w|p> <citta>\"\n", argv[0]);
+        printf("Uso: [..]\n");
         return -1;
     }
 
-    // Inizializzazione Winsock (Necessario solo su Windows)
     #if defined _WIN32
         WSADATA wsa_data;
-        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-            printf("Errore: Fallimento di WSAStartup.\n");
-            return -1;
-        }
+        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) return -1;
     #endif
 
-    // Creazione Socket
     int client_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (client_socket < 0) {
-        perror("Errore creazione socket");
+        perror("Socket error");
         return -1;
     }
 
@@ -63,49 +79,39 @@ int main(int argc, char *argv[]) {
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    // Connessione
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Errore: Impossibile connettersi al server %s:%d.\n", server_ip, port);
-        close(client_socket); // 'close' è mappato a 'closesocket' su Windows grazie a protocol.h
+        printf("Errore connessione a %s:%d\n", server_ip, port);
+        close(client_socket);
         #if defined _WIN32
             WSACleanup();
         #endif
         return -1;
     }
-    printf("Connessione stabilita con %s:%d\n", server_ip, port);
 
-    // Invio Richiesta
-    if (send(client_socket, (char*)&req, sizeof(req), 0) != sizeof(req)) {
-        printf("Errore nell'invio della richiesta.\n");
+    if (send_n(client_socket, (char*)&req, sizeof(req), 0) != sizeof(req)) {
+        printf("Errore invio.\n");
         close(client_socket);
         return -1;
     }
 
-    // Ricezione Risposta
     weather_response_t resp;
-    int bytes_recv = recv(client_socket, (char*)&resp, sizeof(resp), 0);
-
-    if (bytes_recv <= 0) {
-        printf("Errore in ricezione o connessione chiusa dal server.\n");
+    if (recv_n(client_socket, (char*)&resp, sizeof(resp), 0) != sizeof(resp)) {
+        printf("Errore ricezione o timeout.\n");
         close(client_socket);
         return -1;
     }
-
-    // Stampa Risultati
-    printf("Risposta dal server (%s):\n", server_ip);
 
     if (resp.status == 1) {
-        printf(" -> Citta '%s' non disponibile.\n", req.city);
+        printf("Città non disponibile\n");
     } else if (resp.status == 2) {
-        printf(" -> Tipo di richiesta '%c' non valido.\n", req.type);
+        printf("Richiesta non valida\n");
     } else if (resp.status == 0) {
-        printf(" -> %s: ", req.city);
         switch (resp.type) {
-            case 't': printf("Temperatura: %.1f C\n", resp.value); break;
-            case 'h': printf("Umidita: %.1f%%\n", resp.value); break;
-            case 'w': printf("Vento: %.1f km/h\n", resp.value); break;
-            case 'p': printf("Pressione: %.1f hPa\n", resp.value); break;
-            default:  printf("Dato sconosciuto: %.1f\n", resp.value); break;
+            case 't': printf("Temperatura = %.1f C\n", resp.value); break;
+            case 'h': printf("Umidità = %.1f%%\n", resp.value); break;
+            case 'w': printf("Vento = %.1f km/h\n", resp.value); break;
+            case 'p': printf("Pressione = %.1f hPa\n", resp.value); break;
+            default: printf("Dato sconosciuto = %.1f\n", resp.value);
         }
     } else {
         printf("Ricevuto status sconosciuto (%d).\n", resp.status);
